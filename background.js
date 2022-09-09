c589047e1cae539e2b11063651821ef203f6b34b
Vishruth Devan <vishruthdevan@gmail.com>
2022-09-09 21:24:43 +0530

    feat: use tabCapture instead of desktopCapture

--- background.js
+++ background.js
@@ -1,10 +1,6 @@
 chrome.action.onClicked.addListener(function (tab) {
     console.log("works")
-    chrome.desktopCapture.chooseDesktopMedia([
-        "screen",
-        "window",
-        "tab"
-    ], tab, (streamId) => {
+    chrome.tabCapture.getMediaStreamId((streamId) => {
         if (streamId && streamId.length) {
             setTimeout(() => {
                 // chrome.tabs.sendMessage(tab.id, {name: "stream", streamId}, (response) => console.log(response))
